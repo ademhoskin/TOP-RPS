84c21718d5f3a9790719281cf487c647340012fa
Adem Hoskin <ademjhoskin@gmail.com>
2023-05-10 12:40:38 -0400

    finished basic functionality

--- main.js
+++ main.js
@@ -1,2 +1,41 @@
 "use strict";
 
+const getComputerChoice = () => {
+    const choices = ["rock", "paper", "scissors"];
+    const randomNum = Math.floor(Math.random() * 3);
+    return choices[randomNum];
+    };
+
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "You lose! Paper beats rock.";
+        } else {
+            return "You win! Rock beats scissors.";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            return "You lose! Scissors beats paper.";
+        } else {
+            return "You win! Paper beats rock.";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            return "You lose! Rock beats scissors.";
+        } else {
+            return "You win! Scissors beats paper.";
+        }
+    }
+};
+
+const game = () => {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+};
+
+game();
