baf79f7084c39f2058fc7aa4b17e71ebfaa26efc
Adem Hoskin <ademjhoskin@gmail.com>
2023-05-10 12:46:57 -0400

    score counter

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 "use strict";
+let player_score = 0;
+let computer_score = 0;
 
 const getComputerChoice = () => {
     const choices = ["rock", "paper", "scissors"];
@@ -7,26 +9,39 @@ const getComputerChoice = () => {
     };
 
 const playRound = (playerSelection, computerSelection) => {
+
     if (playerSelection === computerSelection) {
         return "It's a tie!";
-    } else if (playerSelection === "rock") {
+    }
+    else if (playerSelection === "rock") {
         if (computerSelection === "paper") {
+            computer_score++;
             return "You lose! Paper beats rock.";
         } else {
+            player_score++;
             return "You win! Rock beats scissors.";
         }
-    } else if (playerSelection === "paper") {
+    }
+    else if (playerSelection === "paper") {
         if (computerSelection === "scissors") {
+            computer_score++;
             return "You lose! Scissors beats paper.";
         } else {
+            player_score++;
             return "You win! Paper beats rock.";
         }
-    } else if (playerSelection === "scissors") {
+    }
+    else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
+            computer_score++;
             return "You lose! Rock beats scissors.";
         } else {
+            player_score++;
             return "You win! Scissors beats paper.";
         }
+    }
+    else {
+        return "Invalid input. Try again.";
     }
 };
 
@@ -35,6 +50,7 @@ const game = () => {
         const playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
+        console.log(`Player: ${player_score} Computer: ${computer_score}`)
     }
 };
 
